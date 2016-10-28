4f35986ad832ae95be59c27a913ab743f88b4247
Snake Man <snakeman@phnet.fi>
2016-10-28 16:29:14 +0300

    Initial commit.

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>WrpTool</title>
+<LINK href="css.css" rel=stylesheet type="text/css">
+<META name="description" content="WrpTool">
+<META name="keywords" content="WrpTool">
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<!--
+<?php include("../include/google-analytics.php"); ?>
+-->
+
+</head>
+<body>
+
+<header>
+<!--
+<?php include("../include/google-ads.php"); ?>
+<?php include("../include/paypal.php"); ?>
+-->
+<h1>WrpTool</h1>
+<p>Welcome to WrpTool.</p>
+</header>
+
+<section>
+<!--
+<a href="arma/index.php">Armed Assault (ArmA)</a><br><br>
+-->
+</section>
+
+<footer>
+<p>Back to <a href="index.php">WrpTool root page</a></p>
+<?php include("../include/paypal.php"); ?>
+<br><br>
+<p><i>PMC WrpTool 2003 - <?print(date("Y"));?>.</i></p>
+<?php include("../include/w3-validator-logo.php"); ?>
+</footer>
+
+</body>
+</html>
