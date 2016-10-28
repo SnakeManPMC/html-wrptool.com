59f2a11b78dd9d8b86cef6a9c75c9386c2738577
Snake Man <snakeman@phnet.fi>
2016-10-28 17:35:18 +0300

    Basic text and stuff working.

---
+++ include/w3-validator-logo.php
@@ -0,0 +1,11 @@
+<p>
+  <a href="http://validator.w3.org/check?uri=referer">
+  <img src="http://www.w3.org/html/logo/badge/html5-badge-h-css3-semantics.png" width="165" height="64" alt="HTML5 Powered with CSS3 / Styling, and Semantics" title="HTML5 Powered with CSS3 / Styling, and Semantics">
+</a>
+
+    <a href="http://jigsaw.w3.org/css-validator/check/referer">
+        <img style="border:0;width:88px;height:31px"
+            src="http://jigsaw.w3.org/css-validator/images/vcss"
+            alt="Valid CSS!">
+    </a>
+</p>
--- index.php
+++ index.php
@@ -1,15 +1,15 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 <head>
 <title>WrpTool</title>
 <LINK href="css.css" rel=stylesheet type="text/css">
-<META name="description" content="WrpTool">
-<META name="keywords" content="WrpTool">
+<META name="description" content="WrpTool The Best Island / Terrain Editor for OFP.">
+<META name="keywords" content="PMC. WrpTool. Island. Terrain. Editor. OFP">
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 
 <!--
-<?php include("../include/google-analytics.php"); ?>
+<?php include("include/google-analytics.php"); ?>
 -->
 
 </head>
@@ -17,25 +17,166 @@
 
 <header>
 <!--
-<?php include("../include/google-ads.php"); ?>
-<?php include("../include/paypal.php"); ?>
+<?php include("include/google-ads.php"); ?>
 -->
 <h1>WrpTool</h1>
 <p>Welcome to WrpTool.</p>
 </header>
 
 <section>
-<!--
-<a href="arma/index.php">Armed Assault (ArmA)</a><br><br>
--->
+<h2>WrpTool - The Best Island / Terrain Editor for OFP</h2>
+<p><a href="images/wrptool-big.jpg"><img src="images/wrptool-small.jpg" width="128" height="128" alt="WrpTool"></a>WrpTool
+</p>
+
+<p>Download WrpTool v0.906 Release (base install) <a href="downloads/WrpTool.v0.906.Release.rar">WrpTool.v0.906.Release.rar</a><br>
+Download WrpTool v0.950 upgrade (exe + manual) <a href="downloads/WrpTool.v0.950.Upgrade.rar">WrpTool.v0.950.Upgrade.rar</a>
+</p>
+
+<p>Download ODOL Explorer v2.0 (MLOD converter) <a href="downloads/ODOL_Explorer_v2.0.rar">ODOL_Explorer_v2.0.rar</a>
+</p>
+
+<p>Notice that first time users need to download both of the above rars, old v0.906 users can download only the upgrade and unrar it to their existing dir to get the v0.95 running. Please, please we plead to you to read the manual...
+</p>
+
+<p>WrpTool system requirements: Windows 2000/XP and OFP (preferably v1.96+).
+</p>
+
+<p>This page is for WrpTool project. First we must thank a-lone-wolf (from <a href="http://web.quick.cz/fholesinsky/OF/">here</a>) for providing us with the wrp file formats, without him there would be no wrptool from us. Thank You, very much appreciated!
+</p>
+
+<p>WrpTool is one util to create OFP Islands / terrains, all you need is notepad + PBO packer and you're capable of doing excellent islands. WrpTool edits all aspects of the basic terrain + objects.
+</p>
+
+<p> This project started Jan 23rd, 2003 (or bit sooner even), first real working wrp island made by us was done this day. Initial team was Rom and Snake Man, Dschulle joined in later. But with sad events regarding MLOD converter and BAS, April 2004 Rom decided to quit from the team. We will miss him. We will try to continue working on this utility so that one day we have program that easily lets user to build and edit OFP Wrp Islands without working for months about it.
+</p>
+
+<p>Current features of the utility:<br>
+&nbsp;- Writes 12.8km x 12.8km, 25.6km x 25.6km and 51.2km x 51.2km WRP files (and smaller).<br>
+&nbsp;- Import terrain in Wilbur long/lat TXT format.<br>
+&nbsp;- Place random 3d objects throughout the map, but not in the water.<br>
+&nbsp;- Graphical User Interface (GUI) to edit 3d object placement.<br>
+&nbsp;- bitmap preview image &quot;icon&quot; of objects in object browser<br>
+&nbsp;- Copy &amp; Paste for object, or multiple objects.<br>
+&nbsp;- Current object preview from bmp image when mouse over object.<br>
+&nbsp;- Object rotation using mouse, multiple or single object.<br>
+&nbsp;- Display 3D-View of the terrain elevations and objects (no texture yet). <br>
+&nbsp;- Graphical User Interface (GUI) to edit elevations and texture placement.<br>
+&nbsp;- XML configuration file for object p3d icons and categories.<br>
+&nbsp;- GUI mini map, for quick overview and jump-to selections.<br>
+&nbsp;- Reads 4WVR, OPRW v2 and OPRW v3 island formats.<br>
+&nbsp;- Rescale object sizes, change their direction.<br>
+&nbsp;- Road Tool. Place roads by clicking RMB for left, right, straight etc road pieces.<br>
+&nbsp;- Region Tool, choose region and random place objects, delete objects etc. <br>
+&nbsp;- Forest Placer, to quickly and automatically place forests, including SEB, BAS 1 obj forests.<br>
+&nbsp;- Automatic texture placing for &quot;transitions&quot;.<br>
+&nbsp;- Cell Selector, to select cells according to elevations.
+</p>
+
+<p>WrpTool release package comes with 124 page PDF manual which contains all the necessary information to successfully operate WrpTool and to create islands from ground up.
+</p>
+
+<p>Future plans, TODO list:<br>
+&nbsp;- Feature to place terrain types.<br>
+&nbsp;- Placer to placer different objects on different terrain types.<br>
+&nbsp;- Templates for cities and airbases for automatic placement.<br>
+&nbsp;- Import MicroDEM .dem format (can be done but not implemented with wilbur txt).<br>
+&nbsp;- Bitmap drawing (ie MAP) from the terrain types and water, and objects.<br>
+&nbsp;- Automatic road placement between two points (cities for example).
+<br><br>
+How <a href="images/wrp.gif">cells</a> are set up. How <a href="images/Forest.gif">forest 3d</a> objects are set up.
+</p>
+
+<p><b>--- Apr 8th, 2004. ODOL Explorer v2.0 release<br>
+</b>ODOL Explorer v2.0 has been released as standalone download. This is the util to view p3d models and to save the ODOL format into MLOD format which can be read and edited with O2. Here is a screenshot of <a href="images/OdEx1.jpg">ODOL Explorer</a> in action. Download ODOL Explorer v2.0 <a href="downloads/ODOL_Explorer_v2.0.rar">here</a>. We will miss Rom since he decided to quit WrpTool development team because he as BAS member cannot stand MLOD converter to be released for the OFP community.
+</p>
+
+<p><b>--- Apr 3rd, 2004. ODOL -&gt; MLOD<br>
+</b>ODOL Explorer has just gotten new feature, its ODOL -&gt; MLOD converter. You can see the results here in O2 views of BAS <a href="images/MLOD1.jpg">MH-60K</a>, BAS <a href="images/MLOD2.jpg">DeltaRanger</a> and BAS <a href="images/MLOD3.jpg">Littlebird</a> helicopter. ODOL Explorer also reads and displays the textures now correctly as seen on this ODOL Explorer image of BAS <a href="images/MLOD4.jpg">UH-60L</a> helo.
+</p>
+
+<p><b></b><b>--- Dec 5th, 2003. v0.95<br>
+</b>So much new stuff, but this is <a href="images/wrptool-50.jpg">forest</a> with the Skew feature. Just leech the new v0.95 release...
+</p>
+
+<p><b>--- Oct 12th, 2003. v0.906 First Public Release!<br>
+</b>Today we finally came to the point where we believed that this utility is ready for public release. Its been long journey here from Jan 23rd, 2003 when this all started. Hope you guys find this util useful.
+</p>
+
+<p><b>--- Sep 28th, 2003. v0.898<br>
+</b>We have been working alot but not much of new features, just tweaks and bits like that. But tonight there is something to show. Finally we got basic Road Tool into the util, this is not yet the automatic tool, but let me show you what can be achieved under 2hours including learning the Road Tool config etc. Here is large 1600x1200 image of <a href="images/wrptool-49.gif">WrpTool Tech Demo NAM</a> islands road system as of now, just about 2hrs of work.
+</p>
+
+<p><b>--- Aug 25th, 2003. v0.877<br>
+</b>Another update, after short break. We can now read the ODOL/MLOD p3d objects, WrpTool has its own 3dview which displays the objects. Let me show you, here first we take a look at Montignac, Everon. Closeup of <a href="images/wrptool-44.gif">Montignac street</a>, wider <a href="images/wrptool-45.gif">shot of the city</a>, then lets jump to <a href="images/wrptool-46.gif">Petrovice, Nogova</a> city shot, this is how the <a href="images/wrptool-47.gif">Nogova bridge</a> looks like and last but not least, <a href="images/wrptool-48.gif">Ia Drang FB</a> shot. 3d view can be rotated, zoomed, panned, flipped etc. There are also quite lot of other smaller features added like Rescaling of objects etc...
+</p>
+
+<p><b>--- Jul 9th, 2003. v0.81<br>
+</b>We have now possibility to read MLOD and ODOL p3d files. This first huge image is <a href="images/wrptool-39.gif">Morton</a> with all the objects visible in their true p3d shape, then lets see <a href="images/wrptool-40.gif">Saint Pierre</a> buildings, <a href="images/wrptool-41.gif">Entre Deux</a> looks OK and how about <a href="images/wrptool-42.gif">Everon airbase</a>, notice the freaky/dense looking forest objects. Finally <a href="images/wrptool-43.gif">Lipany</a> shot. This p3d shape display really helps the precise object placement.
+</p>
+
+<p><b>--- Jul 6th, 2003.<br>
+</b>We are now accepting applications to become a WrpTool beta tester. Send us information of your hardware, windows version (OFP needs to be v1.91), what kind of island are you working on including textures and p3d objects. Finally list as many nice features as you can think of which you would like to see in OFP island creation tool, let your imagination fly. Send email to us if interested.
+</p>
+
+<p><b>--- Jun 25th, 2003. v0.77<br>
+</b>Some minor tweaks to improve the operations like copy-paste etc fixes. But one of the nicer parts here is that WrpTool now reads 4WVR (Visitor export wrp's), OPRW v2 (Malden, Everon, Kolgujev, Intro and WrpEdit wrp's) and also the OPRW v3 (Nogova and binarized wrp's). So yes, currently we dont know any OFP island format that WrpTool could not read. Todays screenshot is from <a href="images/wrptool-38.gif">Mini Map</a> window where you can see island overview and click areas to jump there immediately in the main view. In the screenshot you are looking SEB NAM textures in texture view which zoomed out makes it look dark brown. The small grey stuff is the vegetation objects. Oh btw check the <a href="downloads/wrptool_demo1.rar">WrpTool Tech Demo</a> wrp that was put together in about 4-6hrs, it needs SEB NAM Pack 2 to work (please note that this conflicts with the nam_12 island in our downloads page!).
+</p>
+
+<p><b>--- Jun 23rd, 2003. v0.74<br>
+</b>Finally another update, we have been busy as you can see here. Lets start by that all essential OPRW read feature which allows us to read all BIS islands (nogova needs the hex edit thing) and all user made (WrpEdit) islands. Here is one of my favourites <a href="images/wrptool-31.jpg">SEB NAM Ia Drang</a> made with WrpEdit by SelectThis. Object editing is one thing, but we gotta get to change the elevations too. Here is two images showing you how to edit elevations, in the first image you see <a href="images/wrptool-32.jpg">cells selected for elevation</a> edits. You do this by selecting the Elevation Editing from the top bar and dragging a box around the cells you want to edit. Then you bring up CTRL-E the elevations edit box. Here you can set all the cells to certain height or use relative change like I did here with 2 meters. Little bit of rising and we have the <a href="images/wrptool-33.jpg">terrain edited</a> for this selected area. To edit the terrain textures, just use CTRL-T to bring up <a href="images/wrptool-34.jpg">texture browser</a> shown in the image and you can choose your texture to use, by pointing a mouse to a cell and pressing INSert key you can <a href="images/wrptool-35.jpg">add the selected texture</a> into the cell. Here is nice pic of <a href="images/wrptool-36.jpg">Malden Airbase</a> with the textures showing and last you can see the whole <a href="images/wrptool-37.jpg">Malden Island overview</a> textures.
+</p>
+
+<p><b>--- Jun 9th, 2003. v0.67<br>
+</b>Today we got ourselves working <a href="images/wrptool-29.gif">3D-View</a> of the terrain elevations. The image is rough, the terrain is still not completely 1:1 accurate but it does give nice &quot;bird eye&quot; view of your island. Also make a note that the island in the image is Everon... its read from the OPRW format which is all BIS islands and all WrpEdit user made islands too. We cannot read all of the user made islands yet but we are working on it. For example SEB Ia Drang will not load, sigh. From my island collection, total of 11 islands did not load, while 27 loaded ok. Other features are: Multiple object rotation/movement. <a href="images/wrptool-30.gif">Object Inspector</a> window where you can edit specific details for the selected object and change its type.
+</p>
+
+<p><b>--- Jun 1st, 2003.<br>
+</b>We decided to bring all the <a href="images/wrptool-24.gif">five brothers together</a> in reunion at 51km island. Then we have overview shot of the <a href="images/wrptool-25.gif">South-East Malden</a> with all the objects visibile, gives really nice look whats where etc. But lets <a href="images/wrptool-26.gif">zoom in to Chapoi</a> and take closer look of the objects. The next shows a feature that the OFP community island creators have been dying for lately... First we have <a href="images/wrptool-27.gif">pattern of trees</a> and bushes, then we use mouse to drag a box over them selecting them all, issue CTRL-C key command, do some mouse movements along with CTRL-V and voila, we have just <a href="images/wrptool-28.gif">COPY-PASTEd</a> that tree pattern! Yay.
+</p>
+
+<p><b>--- May 30th, 2003.</b><br>
+Object browser is where we can select 3d objects to place into the map, when one object is selected we point to mouse cursor over the area and ALT-left mouse button press places the object. Its very easy to see the actual <a href="images/wrptool-22.jpg">p3d object graphics</a> in the bitmap preview of object browser.<br>
+<a href="images/wrptool-23.jpg">Closeup of the objects</a> indicates clearly the object rotation, meaning where the object is facing. This is very important to give the terrain vegetation &quot;natural&quot; look.
+</p>
+
+<p><b>--- May 29th, 2003.</b><br>
+This is our new Graphical User Interface (GUI) view of the 51km x 51km island where we imported the official BIS islands <a href="images/wrptool-14.jpg">Malden, Kolgujev and Everon</a> all nicely into one wrp island, notice how tiny the islands are in the large 51km area :) OK lets get <a href="images/wrptool-15.jpg">Everon close-up</a> here. Next we zoom even closer to the city of <a href="images/wrptool-16.jpg">Saint Pierre</a> area where we start to see the objects. Now <a href="images/wrptool-17.jpg">extreme objects close-up</a> where we actually can do some object moving work etc.
+</p>
+
+<p>Now let me see how we can move the objects around. Here is a zoom close-up of my Vietnam 25km islands <a href="images/wrptool-18.jpg">highest mountain</a> area. Notice how the terrain is filled with objects, placed by the wrptool randomly reading a bitmap. Now we want to clear up that high-peak of this mountain, somewhat make it more special place as its empty of crappy vegetation etc rocks. I used a mouse to click an object, then hold left mousebutton down and simply drag the object into different location - easy. This is how the mountain top looks with <a href="images/wrptool-19.jpg">objects moved</a> from it.
+</p>
+
+<p>Our first SEB NAM Pack 2 based test <a href="images/wrptool-12.jpg">25km island in multiplayer</a> test, runs nicely! And check these nicely randomly placed objects with <a href="images/wrptool-13.jpg">random heading</a> (orientation).
+<br><br>
+First experiment to place certain <a href="images/wrptool-09.jpg">objectives per bitmap</a> painting. Trying to make desert rocks... And this if for SEB NAM wrp some village done with random placement but with <a href="images/wrptool-10.jpg">bitmap painting</a>. Same village viewed from <a href="images/wrptool-11.jpg">the editor</a>.
+</p>
+
+<p>Rom did excellent work on the forest routine. Here is <a href="images/wrptool-07.jpg">map image</a> of the bitmap laid forest and also the real thing <a href="images/wrptool-08.jpg">in-game flyby</a> screenshot of the nice trees. Wow that looks nice and it is so fast to do!
+</p>
+
+<p>This aerial image shows <a href="images/wrptool-06.jpg">transition</a> features in work, now the terrain starts to look rather good.
+</p>
+
+<p>With the normal sized map the number of objects were tested; <a href="images/wrptool-05.jpg">925,000 random</a> 3d objects placed all over the (land) map. There was some slight lag on 1.3ghz 512mb ram machine, but still playable just like my old 450mhz, 384mb ram machine.
+</p>
+
+<p>This shot is the first of the fixed 3d <a href="images/wrptool-04.jpg">object placement</a> function where we do not place trees and stuff into the water anymore (more or less).
+</p>
+
+<p>Mission <a href="images/wrptool-01.jpg">editor view</a> of 51.2km x 51.2km island, not all fits in the screen and another image ingame where Cobra is flying <a href="images/wrptool-02.jpg">AI waypoint</a> above ocean. This is first success on BMP <a href="images/wrptool-03.jpg">bitmap painting</a> to place textures in the terrain according to the RGB colors.
+</p>
+
+<p>Next we did some elevations and got tree to &quot;fit to ground&quot; with <a href="images/RomIsGreat5.jpg">32768 object</a> elevation. Then we go wild and place 200,000 <a href="images/RomIsGreat6.jpg">random trees</a> into 25.6km x 25.6km map. And also we did some torture testing on default sized island, with 500,000 <a href="images/RomIsGreat7.jpg">random objects</a> fit to ground.<br>
+</p>
+
+<p>Some screenshots of the the work this util can do. <a href="images/RomIsGreat.jpg">First image</a> at Jan 23rd, 2003, showing just flat terrain with one object - a tree. Next image is bit buggy code, <a href="images/RomIsGreat2.jpg">bunch of trees</a> in a line. Then we get to some lag tests, first <a href="images/RomIsGreat3.jpg">200,000 trees</a> then <a href="images/RomIsGreat4.jpg">500,000 trees</a> all on flat 50m elevation terrain.
+</p>
+
 </section>
 
 <footer>
-<p>Back to <a href="index.php">WrpTool root page</a></p>
-<?php include("../include/paypal.php"); ?>
-<br><br>
 <p><i>PMC WrpTool 2003 - <?print(date("Y"));?>.</i></p>
-<?php include("../include/w3-validator-logo.php"); ?>
+<?php include("include/w3-validator-logo.php"); ?>
 </footer>
 
 </body>
